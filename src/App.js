f6988a3867412f620642fa3060b74ce755403062
antnyk <153430169+antnyk@users.noreply.github.com>
2024-10-29 14:18:30 +0200

    todo1 done (#1)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [task, setTask] = useState('')
+  const [tasks, setTasks] = useState([])
+
+  const addTask = () => {
+    setTasks([...tasks,task]) // adds new task
+    setTask('') //clears input field
+  }
+
+  const deleteTask = (deleted) => {
+    const withoutRemoved = tasks.filter((item) => item !== deleted)
+    setTasks(withoutRemoved)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <h3>Todos</h3>
+      <form>
+        <input 
+          placeholder='Add new task'
+          value={task}
+          onChange={e => setTask(e.target.value)} // updates task when user types
+          onKeyDown={e => {
+            if (e.key === 'Enter'){
+              e.preventDefault()
+              addTask()
+            }
+          }}
+        />
+      </form>
+      <ul>
+          {
+            tasks.map(item => (
+              <li>
+                {item}
+                <button className='deleteButton' onClick={() => deleteTask(item)}>Delete</button>
+              </li> //displays each task in the list
+            ))
+          }
+      </ul>
     </div>
   );
 }
