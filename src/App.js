31a0172fe12bdb3df851252eafa7f1d3d7aea08b
antnyk <anttipnykanen@gmail.com>
2024-10-30 17:14:08 +0200

    todo 3 done

--- src/App.js
+++ src/App.js
@@ -1,50 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [task, setTask] = useState('')
-  const [tasks, setTasks] = useState([])
+	const [task, setTask] = useState("");
+	const [tasks, setTasks] = useState([]);
 
-  const addTask = () => {
-    setTasks([...tasks,task]) // adds new task
-    setTask('') //clears input field
-  }
+	const addTask = () => {
+		setTasks([...tasks, task]); // adds new task
+		setTask(""); //clears input field
+	};
 
-  const deleteTask = (deleted) => {
-    const withoutRemoved = tasks.filter((item) => item !== deleted)
-    setTasks(withoutRemoved)
-  }
+	const deleteTask = (deleted) => {
+		const withoutRemoved = tasks.filter((item) => item !== deleted);
+		setTasks(withoutRemoved);
+	};
 
-  return (
-    <div id="container">
-      <h3>Todos</h3>
-      <form>
-        <input 
-          placeholder='Add new task'
-          value={task}
-          onChange={e => setTask(e.target.value)} // updates task when user types
-          onKeyDown={e => {
-            if (e.key === 'Enter'){
-              e.preventDefault()
-              addTask()
-            }
-          }}
-        />
-      </form>
-      <ul>
-          {
-            tasks.map(item => (
-              <li>
-                {item}
-                <button className='deleteButton' onClick={() => deleteTask(item)}>Delete</button>
-              </li> //displays each task in the list
-            ))
-          }
-      </ul>
-    </div>
-  );
+	return (
+		<div id="container">
+			<h3>Todos</h3>
+			<form>
+				<input
+					placeholder="Add new task"
+					value={task}
+					// e = event
+					onChange={(e) => setTask(e.target.value)} // updates task when user types
+					onKeyDown={(e) => {
+						if (e.key === "Enter") {
+							e.preventDefault();
+							addTask();
+						}
+					}}
+				/>
+			</form>
+			<ul>
+				{tasks.map((item) => (
+					<li>
+						{item}
+						<button className="deleteButton" onClick={() => deleteTask(item)}>
+							Delete
+						</button>
+					</li> //displays each task in the list
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
