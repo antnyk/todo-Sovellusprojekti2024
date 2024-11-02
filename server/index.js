d42c7969da1d0b4ed2e41d680c35b0c5fee27e40
antnyk <153430169+antnyk@users.noreply.github.com>
2024-11-02 12:46:33 +0200

    Todo6 (#6)
    * todo6 is done.
    
    * made a server/tests folder

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
 import { router as todoRouter } from "./routes/todoRouter.js";
+import { router as userRouter } from "./routes/userRouter.js";
 
 const port = process.env.PORT;
 
@@ -9,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", todoRouter);
+app.use("/user", userRouter);
 
 app.use((err, req, res, next) => {
 	const statusCode = err.statusCode || 500;
